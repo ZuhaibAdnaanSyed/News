1d76a3488706668c6c2dec0e01e59bd77425619f
Zuhaib Adnan <zasworksoc@gmail.com>
2023-04-24 20:34:47 +0530

    design

---
+++ index.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.nav-ul');
+
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    menu.classList.toggle('active');
+})
